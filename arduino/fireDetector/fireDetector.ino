3b1dcfb277e67d252962ac2a9b45ce2ae6ab8469
migyeong <rmfosem613@gmail.com>
2022-02-18 18:30:26 -0500

    Update fireDetector.ino

--- arduino/fireDetector/fireDetector.ino
+++ arduino/fireDetector/fireDetector.ino
@@ -2,8 +2,8 @@
 #include <ESP8266HTTPClient.h>
 
 // Connected wifi
-const char* ssid = "wifi ssid";
-const char* password = "wifi password";
+const char* ssid = "IITP";
+const char* password = "K0r3anSquar3!20";
 
 const int buzzerPin = 5; // buzzer sensor
 const int flamePin = 0; // flame sensor
@@ -15,14 +15,15 @@ void passData ()
   
   HTTPClient http;  //Declare an object of class HTTPClient
     
-  String fire="fire";
-  http.begin(client, "http://3.19.6.82:8080/fireAlarm?message="+fire);  //Specify request destination
+  String Fire="FIRE";
+  http.begin(client, "http://3.19.6.82:8080/fireAlarm/testuuid");  //Specify request destination
   int httpCode = http.GET();                                  //Send the request
 
   if (httpCode > 0) { //Check the returning code
 
     String payload = http.getString();   //Get the request response payload
-    Serial.println(payload);             //Print the response payload 
+    Serial.println(payload);             //Print the response payload
+    
   }
   http.end();   //Close connection
 }
@@ -32,13 +33,13 @@ void connectWifi ()
   // start searching to wifi
   WiFi.begin(ssid, password);
 
-  Serial.print("Connecting");
+  Serial.print("Connecting to WiFi");
   // If there is no wifi connection
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.print(".");
   }
-  Serial.print("Connected");  
+  Serial.print("Connected to WiFi");  
 }
 
 void setup () {
@@ -54,18 +55,17 @@ void loop() {
   Flame = digitalRead(flamePin);
 
   // If a flame is detected
-  if (Serial.available() > 0) //Flame == 1
+  if (Flame == 1) // Serial.available() > 0
   {
     passData(); // Send message to server
     digitalWrite(buzzerPin, HIGH); // Sound
-    Serial.println("Fire detection");
-    delay(5000);
+    delay(50000);
     digitalWrite(buzzerPin, LOW);
-    delay(5000);
+    delay(50000);
   }
   else // general situation
   {
-    Serial.println("No problem");
+    passData();
     delay(10000);
     digitalWrite(buzzerPin, LOW);
   }
