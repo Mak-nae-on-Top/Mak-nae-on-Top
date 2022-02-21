25ba5fc56b3c1305c14eb580b9d8e5693269a5c7
거녀리 맥북 <rjsduf@geonyeocBookAir.ksq.lcl>
2022-02-21 18:46:45 -0500

    fix: Modify drawing route

--- frontend/src/Components/Beacon/DrawMap.js
+++ frontend/src/Components/Beacon/DrawMap.js
@@ -2,44 +2,30 @@ import * as React from 'react';
 import axios from 'axios';
 import {Image, View, Dimensions} from 'react-native';
 import ImageZoom from 'react-native-image-pan-zoom';
-import Svg, {Circle, Path, Marker, Polyline} from 'react-native-svg';
-import BottomBar from '../BottomBar';
+import Svg, {Circle, Polyline} from 'react-native-svg';
 import FireAlarm from '../FireAlarm';
 
 import {Url} from '../../ServerURL/url';
 import deviceInfo from '../GetDevice';
+import {AuthContext} from '../AuthContextProvider';
 
 const windowHeight = Dimensions.get('window').height;
 const windowWidth = Dimensions.get('window').width;
 
-const DrawMap = ({
-  floor,
-  uuid,
-  location,
-  blueprint,
-  blueprintSize,
-  isFired,
-}) => {
-  if (blueprint === null) return <></>;
-  const ratio = windowHeight / Number(blueprintSize.height);
+const DrawMap = props => {
+  const {state} = React.useContext(AuthContext);
 
-  React.useEffect(() => {
-    isFired && setDestination('exit');
-  }, [isFired]);
+  if (props.blueprint === null) return <></>;
+  const ratio = windowHeight / Number(props.blueprintSize.height);
 
   const [polyline, setPolyline] = React.useState('');
 
-  const [destination, setDestination] = React.useState('');
-  const getDestination = dst => {
-    setDestination(dst);
-  };
-
   React.useEffect(() => {
-    const getOptimalRoute = async () => {
+    const getOptimalRoute = async dst => {
       await axios
         .post(
           Url + 'app/loadRoute',
-          {uuid: uuid, floor: floor, destination: destination},
+          {uuid: props.uuid, floor: props.floor, destination: dst},
           {
             headers: {
               'Content-Type': 'application/json',
@@ -48,43 +34,51 @@ const DrawMap = ({
           },
         )
         .then(response => {
-          alert(response.data.message);
+          console.log(response);
           if (response.data.status === 'success') {
             let polyline = '';
             response.data.coordinates.map(coor => {
-              return (polyline += `${coor[0]},${coor[1]}`);
+              return (polyline += `${coor.x},${coor.y} `);
             });
             setPolyline(polyline);
           } else {
             setPolyline('');
           }
-          setDestination('');
         })
         .catch(error => {
           console.log(error);
           throw error;
         });
     };
-    destination !== '' && getOptimalRoute();
-  }, [destination]);
+    props.isFired
+      ? getOptimalRoute('exit')
+      : props.destination &&
+        props.destination !== '' &&
+        getOptimalRoute(props.destination);
+  }, [props.destination, props.isFired]);
+
+  const initBeaconSetting = (x, y) => {
+    state.userToken != null && props.getInitLocation({x: x, y: y});
+  };
 
   return (
     <>
-      {isFired ? <FireAlarm /> : <BottomBar getDestination={getDestination} />}
+      {props.isFired ? <FireAlarm /> : <></>}
       <ImageZoom
         cropWidth={windowWidth}
         cropHeight={windowHeight}
-        imageWidth={Number(blueprintSize.width) * ratio}
+        imageWidth={Number(props.blueprintSize.width) * ratio}
         imageHeight={windowHeight}
         minScale={1}
-        enableCenterFocus={false}>
+        enableCenterFocus={false}
+        onClick={e => initBeaconSetting(e.locationX, e.locationY)}>
         <View style={{position: 'absolute', zIndex: 1}}>
           <Image
             style={{
-              width: Number(blueprintSize.width) * ratio,
+              width: Number(props.blueprintSize.width) * ratio,
               height: windowHeight,
             }}
-            source={{uri: blueprint}}
+            source={{uri: props.blueprint}}
             resizeMode="stretch"
           />
         </View>
@@ -92,36 +86,38 @@ const DrawMap = ({
           <Svg
             height={windowHeight}
             width={
-              (windowHeight / Number(blueprintSize.height)) *
-              Number(blueprintSize.width)
+              (windowHeight / Number(props.blueprintSize.height)) *
+              Number(props.blueprintSize.width)
             }
             viewBox={`0 0 ${
-              (windowHeight / Number(blueprintSize.height)) *
-              Number(blueprintSize.width)
+              (windowHeight / Number(props.blueprintSize.height)) *
+              Number(props.blueprintSize.width)
             } ${windowHeight}`}>
             <Circle
               cx={
-                (Number(location[0].x) * windowHeight) /
-                Number(blueprintSize.width)
+                (Number(props.location[0].x) * windowHeight) /
+                Number(props.blueprintSize.width)
               }
               cy={
-                (Number(location[0].y) * windowHeight) /
-                Number(blueprintSize.height)
+                (Number(props.location[0].y) * windowHeight) /
+                Number(props.blueprintSize.height)
               }
               r="10"
               stroke="black"
               strokeWidth="0.5"
               fill="red"
             />
-            {location.slice(1).map(loc => {
+            {props.location.slice(1).map(loc => {
               return (
                 <Circle
+                  key={loc}
                   cx={
-                    (Number(loc.x) * windowHeight) / Number(blueprintSize.width)
+                    (Number(loc.x) * windowHeight) /
+                    Number(props.blueprintSize.width)
                   }
                   cy={
                     (Number(loc.y) * windowHeight) /
-                    Number(blueprintSize.height)
+                    Number(props.blueprintSize.height)
                   }
                   r="7"
                   stroke="black"
@@ -130,28 +126,13 @@ const DrawMap = ({
                 />
               );
             })}
-            {destination !== '' && (
-              <>
-                <Marker
-                  id="Triangle"
-                  viewBox="0 0 10 10"
-                  refX="0"
-                  refY="5"
-                  p
-                  markerUnits="strokeWidth"
-                  markerWidth="4"
-                  markerHeight="3"
-                  orient="auto">
-                  <Path d="M 0 0 L 10 5 L 0 10 z" />
-                </Marker>
-                <Polyline
-                  points={polyline}
-                  fill="red"
-                  stroke="red"
-                  strokeWidth="3"
-                  markerEnd="url(#Triangle)"
-                />
-              </>
+            {polyline !== '' && (
+              <Polyline
+                points={polyline}
+                fill="red"
+                stroke="red"
+                strokeWidth="8"
+              />
             )}
           </Svg>
         </View>
